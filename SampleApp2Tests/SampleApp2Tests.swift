a06c3c15c7ac14828781de1a45f084b35a3094f2
川満 翔 <>
2017-11-15 17:32:26 +0900

    project commit

---
+++ SampleApp2Tests/SampleApp2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  SampleApp2Tests.swift
+//  SampleApp2Tests
+//
+//  Created by 川満 翔 on 2017/11/14.
+//  Copyright © 2017年 川満 翔. All rights reserved.
+//
+
+import XCTest
+@testable import SampleApp2
+
+class SampleApp2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
